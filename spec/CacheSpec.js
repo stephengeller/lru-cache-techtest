ff7ad925549836f717b80099c84c00e28019ae67
stephengeller <stephen.geller@hotmail.com>
2017-10-27 21:15:42 +0100

    initial solution during test, cleaned up

--- spec/CacheSpec.js
+++ spec/CacheSpec.js
@@ -5,28 +5,35 @@ describe("Cache", function() {
 
 	describe("#set", function() {
 		it("adds a key value pair object", function() {
-			cache.set("Stephen", "Unemployed");
-			expect(cache.cacheKeys["Stephen"].arrayIndex).toEqual(0);
-			expect(cache.cacheKeys["Stephen"].order).toEqual(1);
+			cache.set("Stephen", {
+				name: "Stephen",
+				occupation: "Unemployed"
+			});
+			expect(cache.cacheItems[0].Stephen.name).toEqual("Stephen");
+			expect(cache.cacheItems[0].Stephen.occupation).toEqual("Unemployed");
 		});
 
 		it("limits to 5", function() {
-			cache.set("Stephen1", "1");
-			cache.set("Stephen2", "2");
-			cache.set("Stephen3", "3");
-			cache.set("Stephen4", "4");
-			cache.set("Stephen5", "5");
-			cache.set("Stephen6", "6");
-			lastItem = cache.cacheValues.length - 1;
-			expect(cache.cacheValues.length).toEqual(5);
-			expect(cache.cacheValues[lastItem]).toEqual("2");
+			cache.set("key1", "value1");
+			cache.set("key2", "value2");
+			cache.set("key3", "value3");
+			cache.set("key4", "value4");
+			cache.set("key5", "value5");
+			cache.set("key6", "value6");
+			lastItem = cache.cacheItems.length - 1;
+			expect(cache.cacheItems.length).toEqual(5);
+			expect(cache.cacheItems[lastItem].key2).toEqual("value2");
 		});
 	});
 
 	describe("#get", function() {
 		it("returns a real obj", function() {
-			cache.set("Stephen", "Unemployed");
-			expect(cache.get("Stephen")).toEqual("Unemployed");
+			object = {
+				name: "Stephen",
+				occupation: "Unemployed"
+			};
+			cache.set("Stephen", object);
+			expect(cache.get("Stephen")).toEqual(object);
 		});
 
 		it("moves object to front of array", function() {
@@ -35,8 +42,11 @@ describe("Cache", function() {
 			cache.set("C", "3");
 			cache.set("D", "4");
 			cache.set("E", "5");
+			expect(cache.cacheItems[0]).toEqual({ E: "5" });
 			cache.get("C");
-			expect(cache.cacheValues[0].key).toEqual("C");
+			expect(cache.cacheItems[0]).toEqual({ C: "3" });
+			lastItem = cache.cacheItems.length - 1;
+			expect(cache.cacheItems[lastItem]).toEqual({ A: "1" });
 		});
 	});
 });
--- src/Cache.js
+++ src/Cache.js
@@ -1,58 +1,28 @@
 var Cache = function() {
 	this.LIMIT = 5;
-	this.cacheKeys = {};
+	this.cacheItems = [];
 };
 
-// {"A": {
-// 	arrayIndex: 4,
-// 	order: 3
-// }, "B": {
-// 	arrayIndex: 4,
-// 	order: 3
-// }}
-//
-// obj = {}
-//
-// obj['key1'] = {value: 1}
-// obj['key2'] = {value: 2}
-
 Cache.prototype.get = function(key) {
-	// for (i = 0; i < this.cacheValues.length; i++) {
-	// 	if (key === this.cacheValues[i].key) {
-	// 		object = this.cacheValues[i];
-	// 		this.cacheValues.splice(i, 1);
-	// 		this.cacheValues.unshift(object);
-	// 		return this.cacheValues[i].value;
-	// 	}
-	// }
-	// console.log("The key " + key + " does not exist");
-	if (this.cacheKeys[key] != undefined) {
-		arrayIndex = this.cacheKeys[key].arrayIndex;
-		this.cacheKeys.arrayIndex = 1;
-		return this.cacheValues[arrayIndex];
-	} else {
-		alert("undefined!");
+	items = this.cacheItems;
+	for (i = 0; i < items.length; i++) {
+		if (items[i][key] != undefined) {
+			obj = items[i];
+			this.cacheItems.splice(i, 1);
+			this.cacheItems.unshift(obj);
+			return items[i][key];
+		}
 	}
 };
 
-Cache.prototype.set = function(key, value) {
-	this.cacheValues.push(value);
-	arrayIndex = this.cacheValues.length - 1;
-	this.cacheKeys[key] = {
-		arrayIndex: arrayIndex,
-		order: 1,
-		lastUsed: this.currentDate
-	};
-
-	if (this.cacheValues.length > this.LIMIT) {
-		lastItem = this.cacheValues.length - 1;
-		this.cacheValues.splice(lastItem, 1);
-		delete this.cacheKeys[key];
+Cache.prototype.set = function(cacheKey, value) {
+	obj = {};
+	obj[cacheKey] = value;
+	this.cacheItems.unshift(obj);
+	if (this.cacheItems.length > this.LIMIT) {
+		lastItem = this.cacheItems.length - 1;
+		this.cacheItems.splice(lastItem, 1);
 	}
 };
 
 module.exports = Cache;
-
-// Use currentDate to determine least recently used,
-// then order based on date in array
-// SET:
